7906b7e81dea33da2a6d9f59a4ac64c8db5e205a
kwan <kwan@footfallcam.com>
2023-09-19 22:36:58 -0500

    Server side rendering added.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import { SITE_URL } from "./src/data/config";
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
   integrations: [tailwind(), sitemap(), react()],
   site: SITE_URL,
   markdown: {
